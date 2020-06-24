e999fd6ff8b6f5769e3e7937cdbcf6ed4470426a
cbl <lennart.carbe@gmail.com>
2020-06-24 16:00:49 +0200

    default views

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Project Skeleton</title>
     
     <x-styles/>
 </head>
 <body>
+
+    <main>
+        @yield('content')
+    </main>
     
     <x-scripts/>
 </body>
