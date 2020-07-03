0edad306e1388306b9b4df0d4706c9f3f8c45549
cbl <lennart.carbe@gmail.com>
2020-07-03 09:31:11 +0200

    laravel php_cs_fixer rules

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::trans('/__(routes.home)', PageController::class . '@home')->name('home');
-Route::trans('/__(routes.imprint)', PageController::class . '@imprint')->name('imprint');
-Route::trans('/__(routes.datapolicy)', PageController::class . '@datapolicy')->name('datapolicy');
+Route::trans('/__(routes.home)', PageController::class.'@home')->name('home');
+Route::trans('/__(routes.imprint)', PageController::class.'@imprint')->name('imprint');
+Route::trans('/__(routes.datapolicy)', PageController::class.'@datapolicy')->name('datapolicy');
